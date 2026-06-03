0b4b75549a5d6f3d0550077cb1af496aa2437d0c
Tillomaticus <Till.Ikemann@gmail.com>
2026-06-03 17:09:08 +0200

    feat: GPS Scene

---
+++ Assets/Scripts/4-GPS/GPSLocation.cs
@@ -0,0 +1,195 @@
+using System;
+using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
+
+
+public class GPSLocation : MonoBehaviour
+{
+
+    public enum TrackingMode
+    {
+        Idle,
+        Walking,
+        Encounter
+    }
+
+    public TrackingMode _currentTrackingMode;
+
+
+    [SerializeField] TextMeshProUGUI _coordinatesTextField;
+    [SerializeField] TextMeshProUGUI _trackingModeTextField;
+
+    [SerializeField] Image _walkNorthButtonImage;
+    [SerializeField] Image _walkEastButtonImage;
+
+    [SerializeField] bool _useMockService;
+
+    bool _walkNorth;
+    bool _walkEast;
+
+
+    public class PlayerPosition
+    {
+        public float latitude;
+        public float longitude;
+        public float altitude;
+    }
+
+    PlayerPosition _playerPosition;
+
+    PlayerPosition _mockPosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _playerPosition = new PlayerPosition();
+        _mockPosition = new PlayerPosition();
+
+        if (_useMockService)
+            InvokeRepeating("UpdatePlayerPosition", 1f, 1f);
+        else
+            StartCoroutine("StartLocationService");
+
+
+    }
+
+
+    IEnumerator StartLocationService()
+    {
+        Input.location.Start(10, 10);
+        Debug.Log("Status " + Input.location.status);
+
+        _currentTrackingMode = TrackingMode.Walking;
+        _trackingModeTextField.text = "Trackmode " + _currentTrackingMode.ToString();
+
+        if (!Permission.HasUserAuthorizedPermission(Permission.CoarseLocation))
+        {
+            Permission.RequestUserPermission(Permission.FineLocation);
+            yield return new WaitForSeconds(2f);
+        }
+
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.LogError("Location Service not permitted by user!");
+            yield break;
+        }
+
+        int maxWait = 20;
+
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Running)
+        {
+            Debug.Log("Location ready");
+            InvokeRepeating("UpdatePlayerPosition", 1f, 1f);
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Location initialization failed");
+        }
+
+    }
+
+
+    public void ToggleTrackMode()
+    {
+        //get current TrackingMode
+        TrackingMode newTrackMode = _currentTrackingMode;
+
+        //if we are in last tracking mode -> go to first, else just increase 
+        if (_currentTrackingMode == TrackingMode.Encounter)
+            newTrackMode = TrackingMode.Idle;
+        else
+            newTrackMode++;
+
+        SetTrackingMode(newTrackMode);
+    }
+
+    public void SetTrackingMode(TrackingMode trackingMode)
+    {
+        Input.location.Stop();
+
+        _currentTrackingMode = trackingMode;
+        switch (trackingMode)
+        {
+            case TrackingMode.Idle:
+                Input.location.Start(100f, 50f);
+                break;
+
+            case TrackingMode.Walking:
+                Input.location.Start(10f, 10f);
+                break;
+
+            case TrackingMode.Encounter:
+                Input.location.Start(5f, 2f);
+                break;
+        }
+        _trackingModeTextField.text = "Trackmode " + trackingMode.ToString();
+    }
+
+
+    public void SetMockPosition1()
+    {
+        _mockPosition.latitude = 49.41399f;
+        _mockPosition.longitude = 8.65110f;
+        _mockPosition.altitude = 143f;
+    }
+
+    public void ToggleWalkNorth()
+    {
+        _walkNorth = !_walkNorth;
+
+        if (_walkNorth)
+            _walkNorthButtonImage.color = Color.green;
+        else
+            _walkNorthButtonImage.color = Color.white;
+    }
+
+    public void ToggleWalkEast()
+    {
+        _walkEast = !_walkEast;
+
+        if (_walkEast)
+            _walkEastButtonImage.color = Color.green;
+        else
+            _walkEastButtonImage.color = Color.white;
+    }
+
+
+    void UpdatePlayerPosition()
+    {
+        if (_useMockService)
+            GetPlayerPositionFromMock();
+        else
+            GetPlayerPositionFromGPS();
+        _coordinatesTextField.text = "lat: " + _playerPosition.latitude + "\nlong: " + _playerPosition.longitude + "\nalt :" + _playerPosition.altitude;
+    }
+
+    void GetPlayerPositionFromMock()
+    {
+        if (_walkNorth)
+            _mockPosition.latitude += 2 / 111320f;
+        if (_walkEast)
+            _mockPosition.longitude += 2 / 111320f;
+
+        _playerPosition.latitude = _mockPosition.latitude;
+        _playerPosition.longitude = _mockPosition.longitude;
+        _playerPosition.altitude = _mockPosition.altitude;
+    }
+
+    void GetPlayerPositionFromGPS()
+    {
+        _playerPosition.latitude = Input.location.lastData.latitude;
+        _playerPosition.longitude = Input.location.lastData.longitude;
+        _playerPosition.altitude = Input.location.lastData.altitude;
+    }
+}
